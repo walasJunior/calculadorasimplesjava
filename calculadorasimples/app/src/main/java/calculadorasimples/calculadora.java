a471dd47573db23c6ecbf41a92d645aed336b387
walasJunior <88062443+walasJunior@users.noreply.github.com>
2023-03-16 20:23:00 -0300

    Add files via upload

---
+++ calculadorasimples/app/src/main/java/calculadorasimples/calculadora.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadorasimples;
+
+/**
+ *
+ * @author Admin
+ */
+public class calculadora {
+    
+    public double  somar  (double a, double b){
+        return a + b ;
+        
+    }   
+    public double  subtrair (double a, double b){
+        return a - b ;
+        
+    }   
+     public double  divisao (double a, double b){
+        return a / b ;
+     }
+     public double  multiplicacao (double a, double b){
+        return a * b ;
+     }
+}
+
+        
+        
